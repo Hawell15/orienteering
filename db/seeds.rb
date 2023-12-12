2658e740c151cf8c5b56ea29d662b9b80ea07ca8
romanciobanu <romanciobanu@SE-MacBook-Air-1308.local>
2023-12-12 23:08:25 +0200

    club|add index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,3 +70,5 @@ Competition.create("id": 1, "competition_name": 'Diminuare Categorie', "date": '
 Group.create("id": 0, "group_name": 'No Group', "competition_id": 0)
 Group.create("id": 1, "group_name": 'Diminuare Categorie', "competition_id": 1)
 User.create!(email: "test@mail.ru", password: "1234qwe1234", admin: true)
+Club.create("id": 0, "club_name": 'Individual')
+
