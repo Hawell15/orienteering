f63f6ee98c049b36672ba79b0f6f22d0efcbfd12
romanciobanu <romanciobanu@SE-MacBook-Air-1308.local>
2024-02-08 23:06:58 +0200

    parser|update competition form parser

--- test/parsers/competition_form_parser_spec.rb
+++ test/parsers/competition_form_parser_spec.rb
@@ -8,89 +8,90 @@ RSpec.describe CompetitionFormParser, type: :model do
       params = { 'competition_name' => 'Test Comp', 'date(3i)' => '1', 'date(2i)' => '2', 'date(1i)' => '2024',
                  'location' => 'Chisinau', 'country' => 'Moldova', 'distance_type' => 'Sprint', 'wre_id' => '111', 'group_list' => '' }
       competition_form_parser = CompetitionFormParser.new(params)
-      @competition = competition_form_parser.convert
+      expect { @competition = competition_form_parser.convert }
+      .to change { Competition.count }.by(1)
+      .and change { Group.count }.by(0)
+      .and change { Runner.count }.by(0)
+      .and change { Result.count }.by(0)
+      .and change { Club.count }.by(0)
+      .and change { Entry.count }.by(0)
 
-      expect(Group.count).to eq(0)
-      expect(Runner.count).to be_zero
-      expect(Result.count).to be_zero
-      expect(Club.count).to be_zero
-      expect(Competition.count).to eq(1)
-
-      expect(Competition.last.attributes.except('created_at', 'updated_at')).to eq(
+      expect(Competition.last.attributes.except('created_at', 'updated_at').compact).to eq(
         {
+          'id'               => 2,
           'competition_name' => 'Test Comp',
-          'date' => '2024-02-01'.to_date,
-          'location' => 'Chisinau',
-          'country' => 'Moldova',
-          'distance_type' => 'Sprint',
-          'wre_id' => 111,
-          'id' => 1,
-          'checksum' => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-111").to_s
+          'date'             => '2024-02-01'.to_date,
+          'distance_type'    => 'Sprint',
+          'checksum'         => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-111").to_s,
+          'location'         => 'Chisinau',
+          'country'          => 'Moldova',
+          'wre_id'           => 111
         }
       )
       expect(@competition).to eq(Competition.last)
     end
 
-    it 'passes flow with 2 groups' do
+    it 'passes flow with 1 group' do
       params = { 'competition_name' => 'Test Comp', 'date(3i)' => '1', 'date(2i)' => '2', 'date(1i)' => '2024',
                  'distance_type' => 'Sprint', 'group_list' => 'M21' }
       competition_form_parser = CompetitionFormParser.new(params)
-      @competition = competition_form_parser.convert
 
-      expect(Group.count).to eq(1)
-      expect(Runner.count).to be_zero
-      expect(Result.count).to be_zero
-      expect(Club.count).to be_zero
-      expect(Competition.count).to eq(1)
+      expect { @competition = competition_form_parser.convert }
+      .to change { Competition.count }.by(1)
+      .and change { Group.count }.by(1)
+      .and change { Runner.count }.by(0)
+      .and change { Result.count }.by(0)
+      .and change { Club.count }.by(0)
+      .and change { Entry.count }.by(0)
 
-      expect(Competition.last.attributes.except('created_at', 'updated_at')).to eq(
+      expect(Competition.last.attributes.except('created_at', 'updated_at').compact).to eq(
         {
+          'id'               => 2,
           'competition_name' => 'Test Comp',
-          'date' => '2024-02-01'.to_date,
-          'location' => nil,
-          'country' => nil,
-          'distance_type' => 'Sprint',
-          'wre_id' => nil,
-          'id' => 1,
-          'checksum' => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-").to_s
+          'date'             => '2024-02-01'.to_date,
+          'distance_type'    => 'Sprint',
+          'checksum'         => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-").to_s
 
         }
       )
       expect(@competition).to eq(Competition.last)
 
-      expect(Group.all.pluck(:id, :group_name, :competition_id)).to eq(
-        [[1, 'M21', 1]]
-      )
+      expect(Group.last.attributes.except('created_at', 'updated_at').compact).to eq({
+        "id"             => 2,
+        "group_name"     => "M21",
+        "competition_id" => 2
+      })
     end
 
     it 'passes flow with 2 groups' do
       params = { 'competition_name' => 'Test Comp', 'date(3i)' => '1', 'date(2i)' => '2', 'date(1i)' => '2024',
                  'location' => 'Chisinau', 'country' => 'Moldova', 'distance_type' => 'Sprint', 'wre_id' => '111', 'group_list' => 'M21,W21' }
       competition_form_parser = CompetitionFormParser.new(params)
-      @competition = competition_form_parser.convert
 
-      expect(Group.count).to eq(2)
-      expect(Runner.count).to be_zero
-      expect(Result.count).to be_zero
-      expect(Club.count).to be_zero
-      expect(Competition.count).to eq(1)
+      expect { @competition = competition_form_parser.convert }
+      .to change { Competition.count }.by(1)
+      .and change { Group.count }.by(2)
+      .and change { Runner.count }.by(0)
+      .and change { Result.count }.by(0)
+      .and change { Club.count }.by(0)
+      .and change { Entry.count }.by(0)
 
       expect(Competition.last.attributes.except('created_at', 'updated_at')).to eq(
         {
+          'id'               => 2,
           'competition_name' => 'Test Comp',
-          'date' => '2024-02-01'.to_date,
-          'location' => 'Chisinau',
-          'country' => 'Moldova',
-          'distance_type' => 'Sprint',
-          'wre_id' => 111,
-          'id' => 1,
-          'checksum' => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-111").to_s
+          'date'             => '2024-02-01'.to_date,
+          'distance_type'    => 'Sprint',
+          'checksum'         => (Digest::SHA2.new << "Test Comp-2024-02-01-Sprint-111").to_s,
+          'location'         => 'Chisinau',
+          'country'          => 'Moldova',
+          'wre_id'           => 111
         }
       )
       expect(@competition).to eq(Competition.last)
 
-      expect(Group.all.pluck(:id, :group_name, :competition_id)).to eq(
-        [[1, 'M21', 1], [2, 'W21', 1]]
+      expect(Group.all.drop(2).pluck(:id, :group_name, :competition_id)).to eq(
+        [[2, 'M21', 2], [3, 'W21', 2]]
       )
     end
   end
