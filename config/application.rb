5f199dbef081bde95bc58d347c740a452be4245f
Roman Ciobanu <roman.ciobanu@saltedge.com>
2024-02-27 12:58:21 +0200

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 require 'redis'
-require 'active_support/cache/redis_store' # Add this line to explicitly require the redis_store
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +11,7 @@ module Orienteering
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+    config.cache_store = :redis_store, "redis://localhost:6379/0/cache" # Remove the :expires_in option
     config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
