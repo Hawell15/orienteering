5e462ed8558070971522610ff5fec0283f1147f7
Roman Ciobanu <roman.ciobanu@saltedge.com>
2024-02-27 14:08:40 +0200

    update erb_loader

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const erb = require('./loaders/erb');
 
 const environment = require('./environment')
+environment.loaders.prepend('erb', erb);
 
 module.exports = environment.toWebpackConfig()
