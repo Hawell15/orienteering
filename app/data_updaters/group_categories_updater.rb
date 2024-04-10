c6806e42a38d0fb7843c21be64dbe7470dee7f68
Roman Ciobanu <roman.ciobanu@saltedge.com>
2024-04-10 09:40:55 +0300

    category|update rangs for new clasification

--- app/data_updaters/group_categories_updater.rb
+++ app/data_updaters/group_categories_updater.rb
@@ -27,42 +27,42 @@ class GroupCategoriesUpdater
 
   def rang_array
     [
-      [1200, { 4 => 147, 5 => 174, 6 => 209, 7 => 300, 8 => nil, 9 => nil }.compact],
-      [1100, { 4 => 144, 5 => 170, 6 => 204, 7 => 290, 8 => nil, 9 => nil }.compact],
-      [1000, { 4 => 141, 5 => 166, 6 => 199, 7 => 280, 8 => nil, 9 => nil }.compact],
-      [800,  { 4 => 138, 5 => 162, 6 => 194, 7 => 270, 8 => nil, 9 => nil }.compact],
-      [630,  { 4 => 135, 5 => 158, 6 => 189, 7 => 260, 8 => nil, 9 => nil }.compact],
-      [500,  { 4 => 132, 5 => 154, 6 => 184, 7 => 250, 8 => nil, 9 => nil }.compact],
-      [400,  { 4 => 129, 5 => 150, 6 => 179, 7 => 240, 8 => nil, 9 => nil }.compact],
-      [320,  { 4 => 126, 5 => 146, 6 => 174, 7 => 230, 8 => nil, 9 => nil }.compact],
-      [250,  { 4 => 123, 5 => 142, 6 => 170, 7 => 220, 8 => nil, 9 => nil }.compact],
-      [200,  { 4 => 120, 5 => 138, 6 => 166, 7 => 210, 8 => nil, 9 => nil }.compact],
-      [160,  { 4 => 117, 5 => 135, 6 => 162, 7 => 200, 8 => 290, 9 => nil }.compact],
-      [125,  { 4 => 114, 5 => 132, 6 => 158, 7 => 192, 8 => 275, 9 => nil }.compact],
-      [100,  { 4 => 111, 5 => 129, 6 => 154, 7 => 185, 8 => 260, 9 => nil }.compact],
-      [80,   { 4 => 108, 5 => 126, 6 => 150, 7 => 180, 8 => 250, 9 => nil }.compact],
-      [63,   { 4 => 105, 5 => 123, 6 => 146, 7 => 175, 8 => 240, 9 => nil }.compact],
-      [50,   { 4 => 102, 5 => 120, 6 => 142, 7 => 170, 8 => 230, 9 => nil }.compact],
-      [40,   { 4 => 100, 5 => 117, 6 => 138, 7 => 165, 8 => 220, 9 => 300 }.compact],
-      [32,   { 4 => nil, 5 => 114, 6 => 135, 7 => 155, 8 => 200, 9 => 280 }.compact],
-      [25,   { 4 => nil, 5 => 111, 6 => 132, 7 => 150, 8 => 190, 9 => 270 }.compact],
-      [20,   { 4 => nil, 5 => 108, 6 => 129, 7 => 145, 8 => 185, 9 => 260 }.compact],
-      [16,   { 4 => nil, 5 => 105, 6 => 126, 7 => 140, 8 => 180, 9 => 250 }.compact],
-      [13,   { 4 => nil, 5 => 102, 6 => 123, 7 => 135, 8 => 170, 9 => 230 }.compact],
-      [10,   { 4 => nil, 5 => 100, 6 => 120, 7 => 130, 8 => 155, 9 => 200 }.compact],
-      [8,    { 4 => nil, 5 => nil, 6 => 116, 7 => 125, 8 => 150, 9 => 190 }.compact],
-      [6,    { 4 => nil, 5 => nil, 6 => 114, 7 => 120, 8 => 140, 9 => 180 }.compact],
-      [5,    { 4 => nil, 5 => nil, 6 => 111, 7 => 115, 8 => 135, 9 => 170 }.compact],
-      [4,    { 4 => nil, 5 => nil, 6 => 108, 7 => 110, 8 => 125, 9 => 155 }.compact],
-      [3,    { 4 => nil, 5 => nil, 6 => 105, 7 => 108, 8 => 120, 9 => 147 }.compact],
-      [2,    { 4 => nil, 5 => nil, 6 => 103, 7 => 105, 8 => 114, 9 => 142 }.compact],
-      [1,    { 4 => nil, 5 => nil, 6 => 100, 7 => nil, 8 => 105, 9 => 120 }.compact],
-      [0.5,  { 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => 105 }.compact]
+      [1200, { 4 => 145, 5 => 174, 6 => 205, 7 => 300, 8 => nil, 9 => nil }.compact],
+      [1000, { 4 => 142, 5 => 170, 6 => 200, 7 => 286, 8 => nil, 9 => nil }.compact],
+      [800,  { 4 => 139, 5 => 166, 6 => 195, 7 => 272, 8 => nil, 9 => nil }.compact],
+      [630,  { 4 => 136, 5 => 162, 6 => 190, 7 => 260, 8 => nil, 9 => nil }.compact],
+      [490,  { 4 => 133, 5 => 158, 6 => 185, 7 => 248, 8 => nil, 9 => nil }.compact],
+      [380,  { 4 => 130, 5 => 154, 6 => 180, 7 => 240, 8 => nil, 9 => nil }.compact],
+      [300,  { 4 => 127, 5 => 150, 6 => 175, 7 => 232, 8 => nil, 9 => nil }.compact],
+      [240,  { 4 => 124, 5 => 146, 6 => 171, 7 => 224, 8 => nil, 9 => nil }.compact],
+      [190,  { 4 => 121, 5 => 142, 6 => 167, 7 => 216, 8 => nil, 9 => nil }.compact],
+      [155,  { 4 => 117, 5 => 138, 6 => 163, 7 => 208, 8 => 300, 9 => nil }.compact],
+      [125,  { 4 => 115, 5 => 135, 6 => 159, 7 => 200, 8 => 290, 9 => nil }.compact],
+      [100,  { 4 => 112, 5 => 130, 6 => 155, 7 => 192, 8 => 280, 9 => nil }.compact],
+      [82,   { 4 => 109, 5 => 127, 6 => 151, 7 => 184, 8 => 270, 9 => nil }.compact],
+      [65,   { 4 => 106, 5 => 124, 6 => 147, 7 => 176, 8 => 260, 9 => nil }.compact],
+      [51,   { 4 => 103, 5 => 121, 6 => 143, 7 => 170, 8 => 250, 9 => nil }.compact],
+      [40,   { 4 => 100, 5 => 118, 6 => 139, 7 => 164, 8 => 240, 9 => nil }.compact],
+      [31,   { 4 => nil, 5 => 115, 6 => 136, 7 => 158, 8 => 230, 9 => nil }.compact],
+      [24,   { 4 => nil, 5 => 112, 6 => 133, 7 => 152, 8 => 220, 9 => nil }.compact],
+      [19,   { 4 => nil, 5 => 109, 6 => 130, 7 => 147, 8 => 210, 9 => 300 }.compact],
+      [15,   { 4 => nil, 5 => 106, 6 => 127, 7 => 142, 8 => 200, 9 => 280 }.compact],
+      [12,   { 4 => nil, 5 => 103, 6 => 124, 7 => 138, 8 => 190, 9 => 260 }.compact],
+      [10,   { 4 => nil, 5 => 100, 6 => 121, 7 => 134, 8 => 180, 9 => 240 }.compact],
+      [9,    { 4 => nil, 5 => nil, 6 => 118, 7 => 130, 8 => 170, 9 => 220 }.compact],
+      [8,    { 4 => nil, 5 => nil, 6 => 115, 7 => 126, 8 => 160, 9 => 200 }.compact],
+      [7,    { 4 => nil, 5 => nil, 6 => 112, 7 => 122, 8 => 150, 9 => 185 }.compact],
+      [6,    { 4 => nil, 5 => nil, 6 => 109, 7 => 118, 8 => 140, 9 => 170 }.compact],
+      [5,    { 4 => nil, 5 => nil, 6 => 106, 7 => 114, 8 => 130, 9 => 155 }.compact],
+      [4,    { 4 => nil, 5 => nil, 6 => 103, 7 => 110, 8 => 120, 9 => 140 }.compact],
+      [3,    { 4 => nil, 5 => nil, 6 => 100, 7 => nil, 8 => 110, 9 => 130 }.compact],
+      [2,    { 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => 120 }.compact],
+      [1,    { 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => 110 }.compact],
     ]
   end
 
   def get_rang_percents(rang)
-    return [0, {}] if rang < 0.5
+    return {} if rang < 1
 
     rang_array.detect { |row| row.first <= rang }.last
   end
@@ -83,18 +83,12 @@ class GroupCategoriesUpdater
   end
 
   def get_percent_hash(_winner_time)
-    clasa = @group.clasa
+    clasa = @group.clasa.to_i
     return {} if @group.results.size < 3
 
     get_rang_percents(@group.rang).map do |k, v|
-      case clasa
-      when 'MSRM', 'CMSRM', 'Seniori'
-        [k, v] if [4, 5, 6].include?(k)
-      when 'Seniori + Juniori'
-        [k, v] if [4, 5, 6, 7, 8, 9].include?(k)
-      when 'Juniori'
-        [k, v] if [7, 8, 9].include?(k)
-      end
+
+      [k,v] if k >= clasa rescue byebug
     end.compact.to_h
   end
 
@@ -103,15 +97,17 @@ class GroupCategoriesUpdater
     place = res.place
     clasa = @group.clasa
 
-    category_id = if clasa == 'MSRM' && place == 1
+    category_id = if clasa == '2' && place == 1
                     2
                   elsif clasa[/MSRM/] && (1..3).include?(place)
                     3
                   else
                     time_hash.detect { |_k, v| v >= time }&.first || 10
                   end
+    category_id = 10 if category_id > 6 && (Time.now.year - res.runner.dob.year > 18)
 
-    res.update(category_id:)
+
+    res.update(category_id: category_id)
 
     if res.category_id.to_i < res.runner.category_id.to_i ||
        (res.category_id.to_i == res.runner.category_id.to_i && res.date + 2.years > res.runner.category_valid)
