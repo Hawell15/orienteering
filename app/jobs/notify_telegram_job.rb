fc7b5ec98633459c10d6e5b1e7f18ec483081c35
Roman Ciobanu <roman.ciobanu@saltedge.com>
2024-11-09 22:47:18 +0200

    skip notify in telegram

--- app/jobs/notify_telegram_job.rb
+++ app/jobs/notify_telegram_job.rb
@@ -4,8 +4,8 @@ class NotifyTelegramJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    # Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
-      # bot.api.send_message(chat_id: "-1002114647707", text: message)
-    # end
+    Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
+      bot.api.send_message(chat_id: "-1002114647707", text: message)
+    end
   end
 end
